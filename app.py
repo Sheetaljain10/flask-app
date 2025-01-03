f0ea3b867bead8e6f4391bc68c2913539810c3f3
Sheetaljain10 <sheetal.is21@bmsce.ac.in>
2025-01-04 00:14:29 +0530

    Test Jenkins integration

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask! This is my first program"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
